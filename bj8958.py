67afe8d7b94387fedd597a5b3a180ffb635f2372
jse <73538772+jse8406@users.noreply.github.com>
2022-06-20 10:10:48 +0900

    explanation

--- bj8958.py
+++ bj8958.py
@@ -3,7 +3,7 @@ for i in range(num):
     ox_list = list(input())
     score = 0
     score_sum = 0
-    for j in ox_list:
+    for j in ox_list: #파이썬 for문의 j는 ox_list의 요소 하나하나에 대응
         if j =="O":
             score +=1
             score_sum += score
