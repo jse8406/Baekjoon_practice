d8c1992ab9edd436b2c3b14c5229dd0e3c360c01
jse <73538772+jse8406@users.noreply.github.com>
2022-06-22 03:11:56 +0900

    find함수

--- bj10809.py
+++ bj10809.py
@@ -9,9 +9,11 @@ for i in str:
         storage[alpastr.index(i)] = str.index(i)
 for i in range(26):
     print(storage[i], end=" ")
+    
 #모범 답안 훨씬더 짧다   
 word = input()
 alphabet = list(range(97,123))  # 아스키코드 숫자 범위
 
 for x in alphabet :
-    print(word.find(chr(x)), end=" ") 
+    print(word.find(chr(x)), end=" ") # find함수 찾는 문자가 존재 한다면 해당 위치의 index를
+                                      #반환해주고찾는 문자가 존재 하지 않는다면 -1 을 반환합니다.
