5fcec3af0e98c590297c34d3a1259cfc3e61b836
jse <73538772+jse8406@users.noreply.github.com>
2022-06-22 00:20:10 +0900

    find alpabet

---
+++ bj10809.py
@@ -0,0 +1,17 @@
+alpastr = [] # 알파벳 저장
+str = list(input())
+storage = [] # 알파벳 수 만큼 -1 저장
+for i in range(26):
+    storage.append(-1)
+    alpastr.append(chr(i+97))
+for i in str:
+    if storage[alpastr.index(i)] == -1:
+        storage[alpastr.index(i)] = str.index(i)
+for i in range(26):
+    print(storage[i], end=" ")
+#모범 답안 훨씬더 짧다   
+word = input()
+alphabet = list(range(97,123))  # 아스키코드 숫자 범위
+
+for x in alphabet :
+    print(word.find(chr(x)), end=" ") 
