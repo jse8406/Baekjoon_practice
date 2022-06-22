38849b68f9d40fa33a3e910ff80acff0af4b1d0c
jse <73538772+jse8406@users.noreply.github.com>
2022-06-22 10:18:52 +0900

    counter, most_common class

---
+++ bj1157.py
@@ -0,0 +1,13 @@
+from collections import Counter
+word = input()
+word = word.lower()
+char_set = (Counter(word).most_common(2))
+if len(word)==1:
+    print(word.upper())
+elif (char_set[0][1]) == (char_set[1][1]):
+    print("?")
+elif (char_set[0][1]) > (char_set[1][1]):
+    print(char_set[0][0].upper())
+else:
+    print(char_set[1][0].upper())
+    
