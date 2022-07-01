55b76110bf0c3316d7e7176aa94d6a8008af50c2
jse <73538772+jse8406@users.noreply.github.com>
2022-07-02 01:53:21 +0900

    shit

--- canvas.py
+++ canvas.py
@@ -1,11 +1,12 @@
-case = int(input())
-for i in range(case):
-    k = int(input()) #층 0층부터있다
-    n = int(input()) #호 1호부터있다
-    people = []
-    for i in range(n):
-        people.append(i+1)
-    for j in range(k):
-        for i in range(1,n):
-            people[i] += people[i-1]
-    print(people[n-1])
+n = int(input())
+distnc = list(map(int , input().split())) # distance
+cpl =list(map(int, input().split())) # cost per liter
+cost_sum = distnc[0]*cpl[0]
+min_cost = cpl[0]
+if sum(distnc)>10000 or n<2 or n>1000 or max(cpl)>10000:
+    exit()
+for i in range(1, n-1):
+    if min_cost > cpl[i]:
+        min_cost = cpl[i]
+    cost_sum += min_cost * distnc[i]
+print(cost_sum)
