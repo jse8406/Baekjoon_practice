14cbb71c24d5cf03dddfedc909f36b902ffba285
jse <73538772+jse8406@users.noreply.github.com>
2022-07-02 03:07:55 +0900

    O(n^2) to O(n) by break

--- canvas.py
+++ canvas.py
@@ -1,12 +1,19 @@
 n = int(input())
-distnc = list(map(int , input().split())) # distance
-cpl =list(map(int, input().split())) # cost per liter
-cost_sum = distnc[0]*cpl[0]
-min_cost = cpl[0]
-if sum(distnc)>10000 or n<2 or n>1000 or max(cpl)>10000:
-    exit()
-for i in range(1, n-1):
-    if min_cost > cpl[i]:
-        min_cost = cpl[i]
-    cost_sum += min_cost * distnc[i]
-print(cost_sum)
+m = int(input())
+li = [i for i in range(n,m+1)] # elememt count = m-n 
+sosoo = []
+for i in range(m-n+1):
+    count = 0
+    for x in range(1, li[i]+1):
+        if li[i]%x == 0:               
+            count += 1
+            if count > 2:
+                break
+    if count == 2: # 1 and itself
+        sosoo.append(li[i])
+
+if len(sosoo) == 0:
+    print(-1)
+else:
+    print(sum(sosoo))
+    print(min(sosoo))
