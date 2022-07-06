6e4990e811beec7e0f192e5df68b72bc0980d1bc
jse <73538772+jse8406@users.noreply.github.com>
2022-07-06 13:24:27 +0900

    merge sort

---
+++ sort/mergesort.py
@@ -0,0 +1,29 @@
+def mergeSort(list):
+    size = len(list)
+    if size <= 1:
+        return list
+    mid = len(list)//2
+    left = mergeSort(list[:mid])
+    right = mergeSort(list[mid:])
+    merged_list = merge(left, right)
+    return merged_list
+
+def merge(list1, list2):
+    merged = []
+    while len(list1) > 0 and len(list2) > 0:
+        if list1[0] <= list2[0]:
+            merged.append(list1.pop(0))
+        else:
+            merged.append(list2.pop(0))
+    if len(list1)>0:
+        merged += list1
+    else:
+        merged += list2
+    return merged
+
+n = int(input())
+a = []
+for i in range(n):
+    a.append(int(input()))
+print(mergeSort(a))
+   
