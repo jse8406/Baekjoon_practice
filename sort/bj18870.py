cf70f4c9d6aac197cb19dc971ddb1fcecdf4126f
jse <73538772+jse8406@users.noreply.github.com>
2022-07-08 03:49:24 +0900

    dictionary and index function

---
+++ sort/bj18870.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr2 = sorted(list(set(arr)))
+dic = {arr2[i] : i for i in range(len(arr2))} # key = numbers in arr
+                                    # value = key의 값보다 작은 key값의 개수
+for i in arr:
+    print(dic[i], end = ' ')
