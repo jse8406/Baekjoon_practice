1260f8ad6cda6e17c6e9a2f0a7341122269d9140
jse <73538772+jse8406@users.noreply.github.com>
2022-07-07 12:34:15 +0900

    sort function

---
+++ sort/bj11650.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+point_list = []
+for i in range(n):
+    point_list.append(list(map(int, input().split())))
+
+sorted_list = sorted(point_list)
+
+for i in range(n):
+    print(sorted_list[i][0], sorted_list[i][1])
