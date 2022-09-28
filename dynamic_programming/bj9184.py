71596c89f3683781e9f1cdc57d803a0429a04f38
jse <73538772+jse8406@users.noreply.github.com>
2022-09-29 01:36:44 +0900

    comment

--- dynamic_programming/bj9184.py
+++ dynamic_programming/bj9184.py
@@ -4,8 +4,8 @@ def w(a,b,c):
     return 1
   elif a>20 or b>20 or c>20:
     return w(20, 20, 20)
-  elif dp[a][b][c]:
-    return dp[a][b][c]
+  elif dp[a][b][c]: # 만약 필요한 호출 된 값이 있으면 계산하지 않고 저장된 배열에서
+    return dp[a][b][c] # 값을 가져와 그대로 사용
   elif a < b and b < c :
     dp[a][b][c] = w(a,b,c-1) + w(a,b-1,c-1) - w(a,b-1,c) 
     return dp[a][b][c]
