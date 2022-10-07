f023cd5086b84d5335b1052ccbb9f1b65ab50fec
jse <73538772+jse8406@users.noreply.github.com>
2022-10-07 13:42:07 +0900

    slice the string

---
+++ greedy_algorithm/bj1541.py
@@ -0,0 +1,12 @@
+A = list(input().split('-'))
+num = []
+for i in A:
+  cnt = 0
+  s = i.split('+')
+  for j in s:
+    cnt += int(j)
+  num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+  n -= num[i]
+print(n)
