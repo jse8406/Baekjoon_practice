d05eedf68ee081160c3faf2f7138c4585a8d7f28
jse <73538772+jse8406@users.noreply.github.com>
2022-10-08 16:10:51 +0900

    big problem to small problem and memorization

---
+++ dynamic_programming/bj11053.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = list(map(int, input().split()))
+dp = [0] * N
+
+for i in range(N):
+  for j in range(i):
+    if A[j] < A[i] and dp[i] < dp[j]:
+      dp[i] = dp[j]
+  dp[i] += 1
+print(max(dp))
