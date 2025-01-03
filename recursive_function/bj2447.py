f630fa2ebe5dd66fbaa855ae2174f409523de8e9
jse <73538772+jse8406@users.noreply.github.com>
2025-01-03 15:11:24 +0900

    Recursion recap

--- recursive_function/bj2447.py
+++ recursive_function/bj2447.py
@@ -1,17 +1,19 @@
-n = int(input())
+N = int(input())
 
-def star(l):
-    if l == 3:
-        return ['***','* *','***']
-    arr = star(l//3) # 프렉탈 느낌으로 제일 작은 패턴부터 점점 확장
+def star(n):
+    if n == 3:
+        return ['***', '* *', '***']
+    arr = star(n//3)
     stars = []
-
+    
     for i in arr:
         stars.append(i*3)
     for i in arr:
-        stars.append(i+' '*(l//3)+i)
+        stars.append(i+' '*(n//3)+i)
     for i in arr:
         stars.append(i*3)
     return stars
+    
+print('\n'.join(star(N)))
 
-print('\n'.join(star(n)))
+# stars에 작은 패턴부터 저장해놓고 그걸 다시 가져와서 * 연산으로 점점 부풀려 나가는 느낌낌
