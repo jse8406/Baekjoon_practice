56d25fde282051ca732cb8916e07f495a60a8180
jse <73538772+jse8406@users.noreply.github.com>
2025-01-14 10:26:13 +0900

    Use getline funct to get \n

--- string/bj2743.cpp
+++ string/bj2743.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 int main(){
-    char str[100] = {'0',};
-    cin >> str;
-    
-    int answer = 0;
-    cout << str << endl;
-    while(str[answer] != '0'){
-        answer++;
-    }
-    cout << answer;
+    string s;
+    cin >> s;               
+    cout << s.length();      
     return 0;
 }
