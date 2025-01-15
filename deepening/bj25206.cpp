ab20937ae01b2418647f8c2bf5b611622b80236a
jse <73538772+jse8406@users.noreply.github.com>
2025-01-15 17:59:04 +0900

    soon

---
+++ deepening/bj25206.cpp
@@ -0,0 +1,24 @@
+//과목명, 학점, 등급
+
+#include <iostream>
+
+using namespace std;
+
+void main(){
+    string subname, rate;
+    float credit;
+    cin.getline(cin, )
+    return;
+}
+
+float changeScore(string rate){
+    if(rate == "A+") return 4.5;
+    else if(rate == "A0") return 4.0;
+    else if(rate == "B+") return 3.5;
+    else if(rate == "B0") return 3.0;
+    else if(rate == "C+") return 2.5;
+    else if(rate == "C0") return 2.0;
+    else if(rate == "D+") return 1.5;
+    else if(rate == "D0") return 1.0;
+    else if(rate == "F") return 0.0;
+}
