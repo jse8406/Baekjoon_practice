8ee57520a871b7384f155bedcf05152f53dba236
jse <73538772+jse8406@users.noreply.github.com>
2025-01-18 22:47:28 +0900

    sort 3rd arg call back function

--- sort/bj2751.cpp
+++ sort/bj2751.cpp
@@ -5,6 +5,11 @@
 
 using namespace std;
 
+// bool compare(int a, int b){
+//     if(a>b) return true;
+//     else return false;
+// }
+
 
 int main(){
     int N,n;
@@ -14,6 +19,7 @@ int main(){
         cin >> n;
         v.push_back(n);
     }
+    // callback 함수 추가로 내림차순 정렬로 바꿀 수 있음
     sort(v.begin(), v.end());
     for(int i=0;i<N;i++){
         cout << v[i] << '\n';
