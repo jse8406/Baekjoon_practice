309b958acc3be7bdbeb149308d09025070f9940d
jse <73538772+jse8406@users.noreply.github.com>
2025-01-27 20:57:49 +0900

    UPDATE README

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 PS practice for algorithm
 
 [![Solved.ac Profile](http://mazassumnida.wtf/api/generate_badge?boj=ya8406)](https://solved.ac/ya8406)
+
+## Major algorithm
+
+### Sort
+### Greedy
+### DFS/BFS, brute force (완전 탐색)
+### Binary search
+### Dynamic programming
+### Shortest Path
+### Graph Theory
+
--- backtracking/bj14889.cpp
+++ backtracking/bj14889.cpp
@@ -1,59 +1,35 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int stats[21][21];
-bool check[22];
-int N;
-int ans = 1000000000; // 10억
+int init = INT8_MAX;
 
-void DFS(int x, int pos) // x는 카운트 수, pos는 다음 값
-{
-	if (x == N / 2) // 카운트수가 정원의 1/2이 됐을 때 능력치합 계산
-	{
-		int start, link;
-		start = 0;
-		link = 0;
+// pseudo code
+/* 
+	팀 구성을 한명씩 이룬다.
+	n/2 : n/2로 팀이 나눠지면 각 팀의 stat의 합을 계산한다.
+	차를 계산하고 이를 init 변수와 비교해서 더 작으면 갱신한다
+	새로운 팀을 구성 시켜서 위 과정을 반복한다.
+	=> 팀을 새롭게 구성하는 과정 : 
+		그냥 for문 : 브루트 포스
+		사전에 안되는 경우는 탐색 x (pruning) : backtracking
+		backtracking은 주로 재귀로 구현한다.
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				if (check[i] == true && check[j] == true) start += stats[i][j];
-				if (check[i] == false && check[j] == false) link += stats[i][j];
-			}
-		}
+*/
+// DFS에 필요한 args?
 
-		int temp = abs(start - link);
-		if (ans > temp) ans = temp;
+void DFS(){
 
-		return;
-	}
-
-	for (int i = pos; i < N; i++)
-	{
-		check[i] = true;
-        // cout << "DFS(" << x+1 << ", " << i+1 << ")" << endl; 
-        // x arg = when to calculate the stats sum, i arg = start index to search
-		DFS(x + 1, i + 1);
-		check[i] = false;
-	}
+	return;
 
 }
 
-int main()
-{
-	cin >> N;
+int main(){
 
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cin >> stats[i][j];
-		}
-	}
 
-	DFS(0, 1); // 카운트 0회부터 숫자는 1부터 시작
 
-	cout << ans;
+	return 0;
 }
