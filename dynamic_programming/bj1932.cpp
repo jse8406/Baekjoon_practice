2d104d3b409c48f238f3d154930b7ab0f174718b
jse <73538772+jse8406@users.noreply.github.com>
2025-02-06 13:53:01 +0900

    DP recycle

--- dynamic_programming/bj1932.cpp
+++ dynamic_programming/bj1932.cpp
@@ -3,11 +3,33 @@
 
 using namespace std;
 
+int dp[500][500];
+
 int main(){
 
     int N;
     cin >> N;
-    
+    for (int i=0; i<N; i++){
+        for(int j=0;j<=i;j++){
+            cin >> dp[i][j];
+        }
+    }
+
+    for (int i=1; i<N; i++){
+        for(int j=0;j<=i;j++){
+            if(j==0){
+                dp[i][j] += dp[i-1][j]; 
+            }
+            else if(j==i){
+                 dp[i][j] += dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] += max(dp[i-1][j], dp[i-1][j-1]); 
+            }
+        }
+    }
+    int ans = *max_element(dp[N-1], dp[N-1]+N);
+    cout << ans;
 
     return 0;
 
